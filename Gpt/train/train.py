ec150609b507b9de11d3fbe90f6a7353032c6bc0
wansiqing1226 <wansiqing1226@qq.com>
2023-07-13 09:41:24 +0800

    modify after first train

--- Gpt/train/train.py
+++ Gpt/train/train.py
@@ -4,5 +4,3 @@ openai.api_key = "sk-vd2r2GG6tVkv9i2T43A5D1E8AfA141E8B22aB45d5a75Ab8b"
 
 list = openai.FineTune.list()
 print(list)
-
-C:/Users/万/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0/LocalCache/local-packages/Python39/Scripts
